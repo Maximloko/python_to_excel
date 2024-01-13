e1d9627ba0fc3d87e1006788241d51609c8dfd5c
Maximloko <maximloko1@gmail.com>
2024-01-13 10:27:21 +0700

    delete empty rows in 'filtered rows', completed the points 2.1; 2.2; 3.1; 3.2; 3.4-3.4.1.3

--- Create file/main.py
+++ Create file/main.py
@@ -1,5 +1,6 @@
 import openpyxl
 from openpyxl.utils.cell import column_index_from_string
+# openpyxl.worksheet.worksheet_module
 import re
 
 ''' Creating an excel file based on another excel file '''
@@ -8,7 +9,7 @@ sheet = wb['DATA']
 wb.create_sheet(title='filtered rows')
 filtered_rows = wb['filtered rows']
 correct_value = re.compile(r'(\d{13})')
-for row in range(1, sheet.max_row):
+for row in range(2, sheet.max_row):
     if sheet.cell(row=row, column=column_index_from_string('S')).value:
         for groups in correct_value.findall(sheet.cell(row=row, column=column_index_from_string('S')).value):
             filtered_rows['S' + str(row)] = groups
@@ -19,4 +20,57 @@ for row in range(1, sheet.max_row):
             filtered_rows['E' + str(row)] = sheet.cell(row=row, column=column_index_from_string('E')).value
             filtered_rows['C' + str(row)] = sheet.cell(row=row, column=column_index_from_string('C')).value
             filtered_rows['Y' + str(row)] = sheet.cell(row=row, column=column_index_from_string('Y')).value
-wb.save('test.xlsx')
+    else:
+        pass  # пометить строку с пустым значением 'S28'
+sheet = wb['filtered rows']
+# wb.save('test.xlsx')
+empty_rows = [i for i in range(2, sheet.max_row)
+              if sheet.cell(row=i, column=column_index_from_string('S')).value is None]
+for i in reversed(empty_rows):
+    sheet.delete_rows(i)
+result = openpyxl.load_workbook('postbacks-import-template (42).xlsx')
+table = result['Example CSV']
+for row in range(2, sheet.max_row):
+    value_A = sheet.cell(row=row, column=column_index_from_string('A')).value
+    value_S = sheet.cell(row=row, column=column_index_from_string('S')).value
+    if sheet.cell(row=row, column=column_index_from_string('O')).value == 'Declined':
+        table['C'+str(row)] = value_S
+        table['G'+str(row)] = 'Declined'
+        value_N = sheet.cell(row=row, column=column_index_from_string('N')).value
+        table['H'+str(row)] = f'The customer with application number {value_A} was rejected due to {value_N}.'
+    if sheet.cell(row=row, column=column_index_from_string('O')).value == 'Approved':
+        table['C' + str(row)] = value_S
+        table['G' + str(row)] = 'Approved'
+        value_E = sheet.cell(row=row, column=column_index_from_string('E')).value
+        table['H' + str(row)] = f'The customer with application number {value_A} has got {value_E}.'
+    if sheet.cell(row=row, column=column_index_from_string('O')).value == 'IPA':
+        if sheet.cell(row=row, column=column_index_from_string('Y')).value is None:
+            if sheet.cell(row=row, column=column_index_from_string('C')).value == 'STPK':
+                table['C' + str(row)] = value_S
+                table['G' + str(row)] ='Pending'
+                table['H' + str(row)] = f'The client with application number {value_A} has gotten initial approval ' \
+                                        f'from the bank. The client has to complete KYC verification by using this ' \
+                                        f'link: http://www.axisbank.com/vkyc. Ask the client to complete the KYC.'
+            elif sheet.cell(row=row, column=column_index_from_string('C')).value == 'STPI':
+                table['C' + str(row)] = value_S
+                table['G' + str(row)] = 'Pending'
+                table['H' + str(row)] = f'The client with application number {value_A} has gotten initial approval ' \
+                                        f'from the bank. The client has to provide income proof. The bank will ' \
+                                        f'contact the client'
+            elif sheet.cell(row=row, column=column_index_from_string('C')).value == 'STPT':
+                table['C' + str(row)] = value_S
+                table['G' + str(row)] = 'Pending'
+                table['H' + str(row)] = f'The client with application number {value_A} has gotten initial approval' \
+                                        f' from the bank. The client has to provide income proof and KYC verification' \
+                                        f' by using this link: http://www.axisbank.com/vkyc.'
+            else:
+                table['C' + str(row)] = value_S
+                table['G' + str(row)] = 'Pending'
+                table['H' + str(row)] = f'The client with application number {value_A} has gotten initial approval' \
+                                        f' from the bank. The bank will contact the client.'
+
+result.save('test3.xlsx')
+
+
+
+# if __name__ == '__main__':
